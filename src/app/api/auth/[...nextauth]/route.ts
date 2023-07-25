679d96753f09a8177b50c6d38448fcabc456247c
angusmellor <theangusmellor@gmail.com>
2023-07-25 11:10:58 +0100

    route handler

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from '@prisma/client'
+import NextAuth from "next-auth/next";
+import EmailProvider from "next-auth/providers/email";
+import  GoogleProvider from "next-auth/providers/google"
+
+const prisma = new PrismaClient();
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string 
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),  
+})
+
+export { handler as GET, handler as POST };
