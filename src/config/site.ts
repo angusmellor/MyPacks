da856d4a3da010e8e0f71c66304152820a510163
angusmellor <theangusmellor@gmail.com>
2023-10-04 23:53:15 +0100

    site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,19 @@
-const colourPalette = [ 'custBlue', 'custBlue2', 'custGreen', 'custPink', 'custPurp', 'custBrown', 'custOrng']
-const dbName = 'mypacks'
+const colourPalette = [
+  "custBlue",
+  "custBlue2",
+  "custGreen",
+  "custPink",
+  "custPurp",
+  "custBrown",
+  "custOrng",
+];
+const dbName = "mypacks";
 let baseUrl: string;
 
-if (process.env.NODE_ENV === 'production') {
-    baseUrl = '';
+if (process.env.NODE_ENV === "production") {
+  baseUrl = "http://mypacks.io";
 } else {
-    baseUrl = 'http://localhost:3000'; // Development API base URL
+  baseUrl = "http://localhost:3000"; // Development API base URL
 }
 
-export { colourPalette, dbName, baseUrl }
+export { baseUrl, colourPalette, dbName };
